dd7816008dea7c870680f2c0ab907a5f7fb87499
Prakash Raiya <59119176+raiyaprakash@users.noreply.github.com>
2024-10-10 06:10:19 +0530

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -15,7 +15,7 @@ export default {
     let content = await responseClone.text();
 
     // Replace all occurrences of 'www.fastrojgar.com' with 'ipl.fast-rojgar.workers.dev'
-    let modifiedContent = content.replace(/www\.fastrojgar\.com/g, 'ipl.fast-rojgar.workers.dev');
+    let modifiedContent = content.replace(/www\.fastrojgar\.com/g, 'hostweb.pages.dev');
 
     // Return the modified content as a new response
     return new Response(modifiedContent, {
